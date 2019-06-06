30733b33aa6b01c2e0d72c75c0651975d50dd531
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-06 13:21:22 -0400

    Able to create listings and show that listing.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   def index
+    @listing = Listing.new
     @listings = Listing.all
     #raise @listings.inspect #was the controlroller able to get the lists from the db
   end
@@ -7,5 +8,15 @@ class ListingsController < ApplicationController
   def show
     # i need to load the listing
     @listing = Listing.find(params[:id])
+  end
+
+  def create
+    @listing = Listing.create(listing_params)
+    redirect_to listing_url(@listing)
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:title)
   end
 end
