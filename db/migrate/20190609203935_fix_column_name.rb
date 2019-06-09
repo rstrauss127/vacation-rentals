bfe49e3ccedbd9b9ea93112e7f77e4be9d6c8066
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-09 17:01:01 -0400

    Deleted host_id from reservations table. Created form for a new reservation. Next step, have form actually create a reservation

--- db/migrate/20190609203935_fix_column_name.rb
+++ db/migrate/20190609203935_fix_column_name.rb
@@ -1,5 +1,6 @@
 class FixColumnName < ActiveRecord::Migration[5.2]
   def change
     rename_column :reservations, :user_id, :guest_id
+    remove_column :reservations, :host_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_190012) do
+ActiveRecord::Schema.define(version: 2019_06_09_203935) do
 
   create_table "features", force: :cascade do |t|
     t.string "description"
@@ -32,9 +32,8 @@ ActiveRecord::Schema.define(version: 2019_06_09_190012) do
   create_table "reservations", force: :cascade do |t|
     t.datetime "start_date"
     t.datetime "end_date"
-    t.integer "host_id"
     t.integer "listing_id"
-    t.integer "user_id"
+    t.integer "guest_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
