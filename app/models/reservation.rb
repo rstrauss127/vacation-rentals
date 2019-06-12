ce78cc715bd4e0cf0812775d2bcae3860e998bdd
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-12 12:53:15 -0400

    Added model method #guest_email to Reservation model.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :listing
   #has_many :users, through :listing
-
+  def guest_email
+    @user =  User.find(self.user_id)
+    return @user.email
+  end
 end
