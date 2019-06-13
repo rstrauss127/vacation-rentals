9e8e8fafe27877c9932433a38119c6de8163bec0
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-13 13:19:45 -0400

    Listing calender can show individual reservations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
   has_many :reservations #adds methods to model
 
   validates :title, presence: {message: "Must have title"}
-  
-  validates :bedrooms, numericality: true, {message: "Must be a number"}
-  validates :bathrooms, numericality: true, {message: "Must be a number"}
+
+  validates :bedrooms, numericality: {message: "Must be a number"}
+  validates :bathrooms, numericality: {message: "Must be a number"}
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,6 +5,9 @@ class Reservation < ApplicationRecord
 
   #need to write custom validations for :start_date and :end_date
 
+  def start_time
+    self.my_related_model.start_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
 
   def guest_email
     @user =  User.find(self.user_id)
