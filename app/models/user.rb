68bf84d0cf5a0105c4f65b64419252573c189b5a
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-16 18:36:46 -0400

    Signup view will display validations if user does not have an email or password. Added password validation to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :listings
   has_many :reservations, through: :listings
 
   validates :email, presence: {message: "You need an email"}
-  validates :email, uniqueness: {message: "That user exists already"} #make a method that will reroute to login in page if email already exists
+  #validates :email, uniqueness: {message: "That user exists already"} #make a method that will reroute to login in page if email already exists
 
+  validates :password, presence: {message: "Must have a password"}
 
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
