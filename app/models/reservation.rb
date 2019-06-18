6de53383ed0195d5da18bce41a9515cb59535e1f
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-18 13:39:20 -0400

    Added 2 model methods for Reservations, #guest_email and #listing_title. Implemented them in Past Reservations view

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,13 @@ class Reservation < ApplicationRecord
 
   validates :start_date, uniqueness: {message: "That date is booked already"}
 
-
   scope :start_date_before, ->(time) { where("start_date < ?", time) }
+
+  def guest_email
+    User.find(self.user_id).email
+  end
+
+  def listing_title
+    Listing.find(self.listing_id).title
+  end
 end
