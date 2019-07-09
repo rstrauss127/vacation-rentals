8ad1cbccb1f27bf0927cd65946f919b44eb21fa4
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-07-09 16:14:16 -0400

    rolled back db, removed attraction features. removed maps.js assest

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_203050) do
-
-  create_table "attractions", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "type"
-    t.float "distance"
-    t.string "link"
-    t.integer "listing_id"
-    t.string "description"
-  end
+ActiveRecord::Schema.define(version: 2019_07_04_164614) do
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
